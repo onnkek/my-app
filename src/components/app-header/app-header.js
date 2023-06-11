718eb4581cc0c050f24389f996c169182efc0ad8
onnkek <onnkek@gmail.com>
2023-06-11 23:39:34 +0500

    edit file struct v2

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>onnkek</h1>
+            <h2>10 posts, 5 likes</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
